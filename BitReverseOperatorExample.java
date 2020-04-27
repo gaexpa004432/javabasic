ed0e0dae1b0b22dbdc10697640d1dc5af25ffbf9
gaexpa004432 <gaexpa004432@naver.com>
2020-04-27 09:42:28 +0900

    add

---
+++ BitReverseOperatorExample.java
@@ -0,0 +1,25 @@
+public class BitReverseOperatorExample {
+	public static void main (String[] args){
+		int a1 = 10 , a2 = ~a1 , a3 = a2 +1;	
+	
+		
+		String returnValue = toBinaryString(a1);
+		System.out.println(returnValue);
+		String returnValue1 = toBinaryString(a2);
+		System.out.println(returnValue1);
+		String returnValue2 = toBinaryString(a3);
+		System.out.println(returnValue2);
+		int result;
+		result = a1 +a2;
+		System.out.println(result);
+		
+	}
+	public static String toBinaryString(int val){
+		String str = Integer.toBinaryString(val);
+		while(str.length()< 32){
+			str = "0" +str;
+			
+		}
+		return str;
+	}
+}
